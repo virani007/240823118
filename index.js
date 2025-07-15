abdea6cee4e8194f260c324ef2462cff86cf73f4
virani007 <viranimaulik930@gmail.com>
2025-07-15 11:26:51 +0530

    5 programs

--- index.js
+++ index.js
@@ -66,3 +66,27 @@
 //  }
 // console.log(fact)
 
+//6
+// const n=5
+// let mul=1
+// for(i=1;i<=10;i++)
+// {
+//         mul=n*i
+//         console.log(n,"x",i,"=",mul)
+        
+// }
+
+//7
+const n=5;
+let sum=0
+for(i=1;i<=n;i++)
+{
+   sum-=i
+   console.log(i)
+
+}
+console.log(sum)
+
+
+
+
