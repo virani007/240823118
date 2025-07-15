7e96008fbeba28ebd841980aca5460e786e4e50c
virani007 <viranimaulik930@gmail.com>
2025-07-15 11:05:47 +0530

    5 programs

--- index.js
+++ index.js
@@ -1,2 +1,68 @@
-const a="maulik"
-console.log(`hello ${a}`)
+// git clone link
+// git add .
+// git commit -m "message"
+// git push
+// git pull
+
+
+// const a="maulik"
+// console.log(`hello ${a}`)
+
+
+//1
+// const n=10;
+// console.log(n%2);
+// if(n%2==0)
+// {
+//     console.log("Even");
+
+// }else{
+//     console.log("Odd")
+// }
+
+
+//2
+// const n=12
+// const p=10
+// if(n>p)
+// {
+//     console.log("N is big");
+// }
+// else{
+//     console.log("P is big");
+// }
+
+//3
+// const year=2025;
+// if(year%4==0)
+// {
+//     console.log("leap year")
+// }
+// else
+// {
+//     console.log("not leap year")
+// }
+
+//4
+
+// const n=5;
+// let sum=0
+// for(i=1;i<=n;i++)
+// {
+//    sum+=i
+
+// }
+// console.log(sum)
+
+
+
+//5
+
+// const n=5;
+//  let fact=1
+//  for(i=1;i<=n;i++)
+//  {
+//     fact*=i
+//  }
+// console.log(fact)
+
